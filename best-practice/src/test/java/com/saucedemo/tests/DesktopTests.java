542c626699cd5850198a710e72c9c7c5f627c732
Mick Taylor <michael.taylor@uipath.com>
2023-08-10 11:20:16 -0400

    Micks commit.

--- best-practice/src/test/java/com/saucedemo/tests/DesktopTests.java
+++ best-practice/src/test/java/com/saucedemo/tests/DesktopTests.java
@@ -2,6 +2,7 @@ package com.saucedemo.tests;
 
 import com.saucedemo.pages.LoginPage;
 import com.saucedemo.pages.ProductsPage;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -10,31 +11,49 @@ import org.openqa.selenium.TimeoutException;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-/** Desktop Tests. */
+/**
+ * Desktop Tests for SauceDemo website.
+ */
 @RunWith(Parameterized.class)
 public class DesktopTests extends BaseTest {
 
-    @Test()
-    public void loginWorks() {
-        LoginPage loginPage = new LoginPage(driver);
+    private static final String STANDARD_USER = "standard_user";
+    private static final String LOCKED_OUT_USER = "locked_out_user";
+    private static final String INVALID_USER = "foo_bar_user";
+
+    private LoginPage loginPage;
+
+    @Before
+    public void setUp() {
+        // Create a new LoginPage instance before each test
+        loginPage = new LoginPage(driver);
         loginPage.visit();
-        loginPage.login("standard_user");
+    }
+
+    /**
+     * Validates that a standard user can log in successfully.
+     */
+    @Test
+    public void loginWorks() {
+        loginPage.login(STANDARD_USER);
         assertTrue(new ProductsPage(driver).isDisplayed());
     }
 
+    /**
+     * Validates that a locked out user cannot log in.
+     */
     @Test(expected = TimeoutException.class)
     public void lockedOutUser() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.visit();
-        loginPage.login("locked_out_user");
+        loginPage.login(LOCKED_OUT_USER);
         assertFalse(new ProductsPage(driver).isDisplayed());
     }
 
+    /**
+     * Validates that invalid credentials prevent login.
+     */
     @Test(expected = TimeoutException.class)
     public void invalidCredentials() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.visit();
-        loginPage.login("foo_bar_user");
+        loginPage.login(INVALID_USER);
         assertFalse(new ProductsPage(driver).isDisplayed());
     }
 }
