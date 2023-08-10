eb21e14192255cde793dc07376841fdb63628fce
Mick Taylor <michael.taylor@uipath.com>
2023-08-10 19:45:06 -0400

    Added White House Website. Updated Pom and Base Test

--- best-practice/src/test/java/com/saucedemo/tests/BaseTest.java
+++ best-practice/src/test/java/com/saucedemo/tests/BaseTest.java
@@ -16,11 +16,13 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.logging.Logger;
 
 import static com.saucedemo.Constants.*;
 
 public class BaseTest {
     protected RemoteWebDriver driver;
+    private static final Logger LOGGER = Logger.getLogger(BaseTest.class.getName());
 
     protected String WEB_URL = "https://www.saucedemo.com/";
 
@@ -35,92 +37,101 @@ public class BaseTest {
 
     @Parameterized.Parameters()
     public static Collection<Object[]> crossPlatformData() {
-        return Arrays.asList(new Object[][] {
-                { "desktop", "safari", "latest", "macOS 11.00" },
-                { "desktop", "chrome", "latest-1", "macOS 13" },
-                { "desktop", "firefox", "latest", "Windows 11" },
-                { "desktop", "chrome", "latest", "Windows 10" }
+        return Arrays.asList(new Object[][]{
+                {"desktop", "safari", "latest", "macOS 11.00"},
+                {"desktop", "chrome", "latest-1", "macOS 13"},
+                {"desktop", "firefox", "latest", "Windows 11"},
+                {"desktop", "chrome", "latest", "Windows 10"}
         });
     }
+
     @Rule
     public TestName name = new TestName();
 
     @Before
     public void setup() throws MalformedURLException {
+        LOGGER.info("Setting up the test");
+        URL url = getSauceURL();
 
-        System.out.println("BeforeMethod hook");
-        URL url;
-
-        switch (region) {
-            case "us":
-                url = new URL(SAUCE_US_URL);
-                break;
-            case "eu":
-            default:
-                url = new URL(SAUCE_EU_URL);
-                break;
-        }
-
-        boolean isBuildCap = false;
-        MutableCapabilities caps = new MutableCapabilities();
-        MutableCapabilities sauceOptions = new MutableCapabilities();
-
-        switch (platform) {
-            case "desktop":
-                caps.setCapability("browserName", browserDeviceName);
-                caps.setCapability("browserVersion", browserPlatformVersion);
-                caps.setCapability("platformName", platformName);
-                break;
-            case "android":
-                caps.setCapability("platformName", "android");
-                caps.setCapability("appium:automationName", "UiAutomator2");
-                caps.setCapability("browserName", "chrome");
-                caps.setCapability("appium:deviceName", browserDeviceName);
-                caps.setCapability("appium:platformVersion", browserPlatformVersion);
-                break;
-            case "ios":
-                caps.setCapability("platformName", "iOS");
-                caps.setCapability("appium:automationName", "XCuiTest");
-                caps.setCapability("browserName", "safari");
-                caps.setCapability("appium:deviceName", browserDeviceName);
-                caps.setCapability("appium:platformVersion", browserPlatformVersion);
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + platform);
-        }
-
-        sauceOptions.setCapability("username", System.getenv("SAUCE_USERNAME"));
-        sauceOptions.setCapability("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
-        sauceOptions.setCapability("name", name.getMethodName());
-
-        if (!isBuildCap) { //handle build cap
-            LocalDateTime dateTime = LocalDateTime.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH");
-            String buildLocal = "sauceDemo-" +dateTime.format(formatter);
-            String buildVal = System.getenv("BUILD_TAG");
-            sauceOptions.setCapability("build", buildVal == null ? buildLocal : buildVal);
-        }
-
-        caps.setCapability("sauce:options", sauceOptions);
-
+        MutableCapabilities caps = initializeCapabilities();
         try {
             driver = new RemoteWebDriver(url, caps);
-
         } catch (Exception e) {
-            System.out.println("*** Problem to create the driver " + e.getMessage());
+            LOGGER.severe("Problem to create the driver: " + e.getMessage());
             throw new RuntimeException(e);
         }
     }
 
+    private URL getSauceURL() throws MalformedURLException {
+        switch (region) {
+            case "us":
+                return new URL(SAUCE_US_URL);
+            case "eu":
+            default:
+                return new URL(SAUCE_EU_URL);
+        }
+    }
+
+    private MutableCapabilities initializeCapabilities() {
+        MutableCapabilities caps = new MutableCapabilities();
+        switch (platform) {
+            case "desktop":
+                initializeDesktopCapabilities(caps);
+                break;
+            case "android":
+                initializeAndroidCapabilities(caps);
+                break;
+            case "ios":
+                initializeIOSCapabilities(caps);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected platform: " + platform);
+        }
+        setSauceOptions(caps);
+        return caps;
+    }
+
+    private void initializeDesktopCapabilities(MutableCapabilities caps) {
+        caps.setCapability("browserName", browserDeviceName);
+        caps.setCapability("browserVersion", browserPlatformVersion);
+        caps.setCapability("platformName", platformName);
+    }
+
+    private void initializeAndroidCapabilities(MutableCapabilities caps) {
+        caps.setCapability("platformName", "android");
+        caps.setCapability("appium:automationName", "UiAutomator2");
+        caps.setCapability("browserName", "chrome");
+        caps.setCapability("appium:deviceName", browserDeviceName);
+        caps.setCapability("appium:platformVersion", browserPlatformVersion);
+    }
+
+    private void initializeIOSCapabilities(MutableCapabilities caps) {
+        caps.setCapability("platformName", "iOS");
+        caps.setCapability("appium:automationName", "XCuiTest");
+        caps.setCapability("browserName", "safari");
+        caps.setCapability("appium:deviceName", browserDeviceName);
+        caps.setCapability("appium:platformVersion", browserPlatformVersion);
+    }
+
+    private void setSauceOptions(MutableCapabilities caps) {
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("username", System.getenv("SAUCE_USERNAME"));
+        sauceOptions.setCapability("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
+        sauceOptions.setCapability("name", name.getMethodName());
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH");
+        String buildLocal = "sauceDemo-" + dateTime.format(formatter);
+        String buildVal = System.getenv("BUILD_TAG");
+        sauceOptions.setCapability("build", buildVal == null ? buildLocal : buildVal);
+        caps.setCapability("sauce:options", sauceOptions);
+    }
 
     @Rule
     public TestRule watcher = new TestWatcher() {
-
         @Override
         protected void succeeded(Description description) {
-            if(driver != null)
-            {
-                System.out.println("Test Passed!");
+            if (driver != null) {
+                LOGGER.info("Test Passed!");
                 driver.executeScript("sauce:job-result=passed");
                 driver.quit();
             }
@@ -128,15 +139,12 @@ public class BaseTest {
 
         @Override
         public void failed(Throwable e, Description description) {
-            if(driver != null)
-            {
-                System.out.println("Test Failed!");
+            if (driver != null) {
+                LOGGER.severe("Test Failed: " + e.getMessage());
                 driver.executeScript("sauce:job-result=failed");
-                driver.executeScript("sauce:context=" +e.getMessage());
+                driver.executeScript("sauce:context=" + e.getMessage());
                 driver.quit();
             }
         }
-
     };
-
 }
---
+++ selenium-testng-examples/src/test/java/com/saucedemo/selenium/testng/demo/WhiteHouseWebsiteTest.java
@@ -0,0 +1,63 @@
+package com.saucedemo.selenium.testng.demo;
+
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Example of running a TestNG test of the White House website without using Sauce Bindings.
+ */
+public class WhiteHouseWebsiteTest {
+    protected RemoteWebDriver driver;
+
+    @BeforeMethod
+    public void setup(Method method) throws MalformedURLException {
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("username", System.getenv("SAUCE_USERNAME"));
+        sauceOptions.setCapability("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
+        sauceOptions.setCapability("name", method.getName());
+        sauceOptions.setCapability("browserVersion", "latest");
+
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("sauce:options", sauceOptions);
+        URL url = new URL("https://ondemand.us-west-1.saucelabs.com/wd/hub");
+
+        driver = new RemoteWebDriver(url, options);
+    }
+
+    @Test
+    public void testWhiteHouseBriefingRoomLink() {
+        driver.navigate().to("https://www.whitehouse.gov/");
+
+        // You can verify the title of the website if required.
+        // Assert.assertEquals("White House", driver.getTitle());
+
+        driver.findElement(By.linkText("Briefing Room")).click();
+
+        // Wait for a few seconds for visual confirmation
+        // (usually, explicit or implicit waits are used in real-world scenarios)
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Additional TestNG assertions can be added here as required.
+    }
+
+    @AfterMethod
+    public void teardown(ITestResult result) {
+        String status = result.isSuccess() ? "passed" : "failed";
+        driver.executeScript("sauce:job-result=" + status);
+        driver.quit();
+    }
+}
